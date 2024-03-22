eba3fd00896497f923507ba1849e6a1d56800d66
Ryan Saffel <rks023@email.latech.edu>
2024-03-21 20:21:18 -0500

    Ball Shooting Basics Done

--- LATech-Game-Design-Team-10-Pinball-Demo-main/Assets/Scripts/Ball.cs
+++ LATech-Game-Design-Team-10-Pinball-Demo-main/Assets/Scripts/Ball.cs
@@ -1,26 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
 public class Ball : MonoBehaviour
+
+
 { public Controls controls;
+    public float launchForce;
+    public Ball ball;
+    private Rigidbody rb;
+    public float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         controls = new();
         controls.Enable();
     }
 
-    public void Launch()
+    public void Shooter()
     {
-        
+        float actualLaunchForce = Random.Range(launchForce * 0.8f, launchForce * 1.2f);
+        rb.AddForce(Vector3.forward * actualLaunchForce, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (controls.Testing.BallShooter.WasReleasedThisFrame()){
-            // something
+            Shooter();
         }
     }
 }
