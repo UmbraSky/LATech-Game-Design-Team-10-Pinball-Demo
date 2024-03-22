b530afa12cd58a186ae95242b6aa43ac41433e57
Ryan Saffel <rks023@email.latech.edu>
2024-03-21 19:26:12 -0500

    Testing things, Cherry help
    Cherry helped show how PlayerInput is used, I can use it better now

--- LATech-Game-Design-Team-10-Pinball-Demo-main/Assets/Scripts/Ball.cs
+++ LATech-Game-Design-Team-10-Pinball-Demo-main/Assets/Scripts/Ball.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
-{
+{ public Controls controls;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controls = new();
+        controls.Enable();
     }
 
     public void Launch()
@@ -18,6 +19,8 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (controls.Testing.BallShooter.WasReleasedThisFrame()){
+            // something
+        }
     }
 }
