bb0542ab104dd7077881fff56d667a7341b4dd74
Ryan Saffel <rks023@email.latech.edu>
2024-03-24 20:22:14 -0500

    Replaced Duplicate Assets folder
    Less wasted space with correct Assets folder inside of the project

--- LATech-Game-Design-Team-10-Pinball-Demo-main/Assets/Scripts/Bumpers.cs
+++ LATech-Game-Design-Team-10-Pinball-Demo-main/Assets/Scripts/Bumpers.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class Bumpers : MonoBehaviour
 {
     /// private fields
-    private Renderer render;
+    private Renderer ren;
     private float t;
+    private readonly Color ColorHit = Color.green;
+    private readonly Color ColorDefault = Color.white;
 
     // Life Cycle methods
     private void Start() {
-        render = GetComponent<Renderer>();
+        ren = GetComponent<Renderer>();
         t = 0;
     }
     private void Update() {
+        // transition color back to default after being hit
         if (t > 0) {
-            Color color = Color.Lerp(Color.black, Color.red, t);
-            render.material.SetColor("_BaseColor", color);
+            Color color = Color.Lerp(ColorDefault, ColorHit, t);
+            ren.material.SetColor("_BaseColor", color);
+            // the divided by 2 just slows it down some
             t -= Time.deltaTime / 2.0f;
             if (t < 0) {
                 t = 0;
@@ -26,8 +30,10 @@ public class Bumpers : MonoBehaviour
 
     // Other methods
     public void Bump() {
-        print("Bump!");
-        render.material.SetColor("_BaseColor", Color.red);
+        // change to hit color
+        ren.material.SetColor("_BaseColor", ColorHit);
+
+        // allow Update method to transition it slowly back to default color
         t = 1;
     }
 }
